ed023ee500daf99daf2e344de72d4b7298f02bd1
zedaann <zninri@gmail.com>
2026-01-19 02:37:38 +0800

    delete weightloss

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Motivation from "./pages/Motivation";
-import WeightLoss from "./pages/WeightLoss";
+import WeightLoss from "./pages/WeightLoss.pdf";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
